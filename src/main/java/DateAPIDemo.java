e28af99e38efaec317118ead9633d78b43c250b8
Nagoor-K <p.nagoor@axelor.com>
2022-01-18 12:12:05 +0530

    date ap practce

---
+++ src/main/java/DateAPIDemo.java
@@ -0,0 +1,23 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class DateAPIDemo {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+	  //to use current date
+	  LocalDate d= LocalDate.now();
+	  System.out.println("Today date is "+d);
+	  
+	  //to find current time
+	  LocalTime t = LocalTime.now();
+	  System.out.println("the current time is "+t);
+	  
+	  //to print in a particular format
+	  Month month = d.getMonth();
+	    int day = d.getDayOfMonth();
+	    System.out.println("Month : "+month+" day : "+day);
+	    
+  }
+}
