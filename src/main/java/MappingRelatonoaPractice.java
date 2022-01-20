3fa8ed7fb39ce6d7d3ff672aea839a440ffed481
Nagoor-K <p.nagoor@axelor.com>
2022-01-20 18:27:26 +0530

    One To One Mappimg

---
+++ src/main/java/MappingRelatonoaPractice.java
@@ -0,0 +1,33 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class MappingRelatonoaPractice {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+	  
+	  Student student=new Student();
+	  student.setSid(161074);
+	  student.setSname("Patan Nagoor Khan");
+	  student.setMark(90);
+	  
+	  
+	  Laptop laptop=new Laptop();
+	  laptop.setLid(101);
+	  laptop.setLname("HP");
+	  student.setLaptop(laptop);
+	  
+	  Configuration con=new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Laptop.class);
+	  SessionFactory sf=con.buildSessionFactory();
+	  Session session=sf.openSession();
+	  Transaction tx=session.beginTransaction();
+	  
+	  session.save(student);
+	  session.save(laptop);
+	  
+	  tx.commit();
+	  
+  }
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,37 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Student {
+	@Id
+	private int sid;
+	private String sname;
+	private int mark;
+	@OneToOne
+	private Laptop laptop;
+	
+  public int getSid() {
+  return sid;}
+  public void setSid(int sid) {
+  this.sid = sid;}
+  
+  public String getSname() {
+  return sname;}
+  public void setSname(String sname) {
+  this.sname = sname;}
+  
+  public int getMark() {
+  return mark;}
+  public void setMark(int mark) {
+  this.mark = mark;}
+  
+	public Laptop getLaptop() {
+	return laptop;}
+	public void setLaptop(Laptop laptop) {
+	this.laptop = laptop;}
+  
+  
+	
+	
+}
